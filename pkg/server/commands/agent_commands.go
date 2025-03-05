f61b179a49e7f5d4e82bbcb9035d43b7719c7241
supercat7 <superuser3344@protonmail.com>
2025-03-04 22:39:35 -0500

    cont

--- pkg/server/commands/agent_commands.go
+++ pkg/server/commands/agent_commands.go
@@ -1,1 +1,6 @@
 package commands
+
+var AGENT_SHELL_COMMANDS = map[string][]string{
+	"help": {"Prints commands", "Options: help <command> Lists more information about a command"},
+	"exit": {"Exits agent shell", "Exits agent shell"},
+}
